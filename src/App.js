81cd2014639bbbac13b5903b192a8fb34fba25c7
Umair Hoda <iqbalumairi@gmail.com>
2023-03-02 16:11:04 -0500

    Added glassmorphism effect on card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Typography, Card, Avatar } from "@mui/material";
+import { Typography, Card, Avatar, Button } from "@mui/material";
 import "./App.css";
 
 import { createTheme, ThemeProvider } from "@mui/material";
@@ -6,6 +6,7 @@ import SchoolIcon from "@mui/icons-material/School";
 import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 function App() {
   const theme = createTheme({
@@ -17,8 +18,25 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="app">
+        <div className="projects">
+          <Typography variant="h1" sx={{ zIndex: 999 }}>
+            Hi, there my name is Umair Iqbal
+          </Typography>
+        </div>
         <div className="intro">
-          <Card className="main-card" elevation={5} sx={{ borderRadius: 10 }}>
+          <div class="background">
+            <div class="shape"></div>
+            <div class="shape"></div>
+          </div>
+          <Card
+            className="main-card"
+            elevation={5}
+            sx={{
+              borderRadius: 10,
+              backgroundColor: "rgba(255,255,255,0.13)",
+              backdropFilter: "blur(10px)",
+            }}
+          >
             <div className="main-card-root">
               <div className="main-card-root-left-pane">
                 <Avatar
@@ -31,20 +49,64 @@ function App() {
                   Umair Iqbal
                 </Typography>
 
-                <SchoolIcon />
-                <WorkHistoryIcon />
+                <Typography
+                  color={"white"}
+                  sx={{ fontFamily: "roboto", marginBottom: 3, marginTop: 1 }}
+                >
+                  Web and Mobile Development
+                </Typography>
+
+                <Button sx={{ color: "white" }} startIcon={<WorkHistoryIcon />}>
+                  Experience
+                </Button>
+                <Button sx={{ color: "white" }} startIcon={<SchoolIcon />}>
+                  Education
+                </Button>
+
+                <Button sx={{ color: "white" }} startIcon={<SchoolIcon />}>
+                  Resume
+                </Button>
 
                 <div className="contact-info">
-                  <LinkedInIcon />
-                  <GitHubIcon />
+                  <a href="https://www.linkedin.com/in/umair-iqbal-4a1535152/">
+                    <LinkedInIcon fontSize="large" sx={{ color: "white" }} />
+                  </a>
+
+                  <a href="https://github.com/Umair-Iqbal-debug">
+                    <GitHubIcon fontSize="large" sx={{ color: "white" }} />
+                  </a>
+                  <InstagramIcon fontSize="large" sx={{ color: "white" }} />
                 </div>
+              </div>
+              <div className="main-card-root-right-pane">
+                <Typography color="white" variant="h3" sx={{ marginBottom: 4 }}>
+                  &lt;Hello World/&gt; &#x1F44B;&#x2757;
+                </Typography>
+
+                <Typography
+                  fontSize={20}
+                  fontFamily={"roboto"}
+                  color={"whitesmoke"}
+                >
+                  Hi, there my name is Umair Iqbal, I am passionate about
+                  solving problems through Software Development. I am a junior,
+                  majoring in Computer Science at Stony Brook University,
+                  expected to graduate in Spring 2024.
+                </Typography>
+
+                <Typography
+                  fontSize={20}
+                  fontFamily={"roboto"}
+                  color={"whitesmoke"}
+                >
+                  You can click on the links on the left to explore my projects,
+                  resume or previous work experiences. I am always eager to
+                  collaborate on an interesting project or talk about a project
+                  I have made before so feel free to contact me via email.
+                </Typography>
               </div>
             </div>
           </Card>
-        </div>
-
-        <div className="projects">
-          <Typography variant="h1">Hi, there my name is Umair Iqbal</Typography>
         </div>
       </div>
     </ThemeProvider>
