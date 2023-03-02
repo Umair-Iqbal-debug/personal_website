3bff38039dbaf0c9ac008f1e8d7cb7f8aece4bf7
Umair Hoda <iqbalumairi@gmail.com>
2023-03-01 22:04:17 -0500

    Finally started - added main card need to change colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Card, Avatar } from "@mui/material";
+import "./App.css";
+
+import { createTheme, ThemeProvider } from "@mui/material";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Tilt Warp", "cursive"].join(","),
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <div className="intro">
+          <Card className="main-card" elevation={5} sx={{ borderRadius: 10 }}>
+            <div className="main-card-root">
+              <div className="main-card-root-left-pane">
+                <Avatar
+                  src="./images/umair_port.jpg"
+                  alt="Umair Iqbal"
+                  sx={{ width: 200, height: 200 }}
+                />
+
+                <Typography color={"white"} variant="h4">
+                  Umair Iqbal
+                </Typography>
+
+                <SchoolIcon />
+                <WorkHistoryIcon />
+
+                <div className="contact-info">
+                  <LinkedInIcon />
+                  <GitHubIcon />
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+
+        <div className="projects">
+          <Typography variant="h1">Hi, there my name is Umair Iqbal</Typography>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
