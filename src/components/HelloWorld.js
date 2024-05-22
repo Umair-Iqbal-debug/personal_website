1d3b71a6f2e9a78c0937e674e50da7d7769b3c7e
Umair Hoda <iqbalumairi@gmail.com>
2024-05-22 17:38:16 -0400

    Update HelloWorld.js

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -7,30 +7,26 @@ export default function HelloWorld() {
       <h2 className="titles">&lt;Hello World/&gt; &#x1F44B;!</h2>
       <div className="main-card-root-right-pane-main-text">
         <p className="text">
-          Hi, there my name is <b>Umair Iqbal</b>, I am passionate about solving
-          problems through Software Development. I am a junior, majoring in
-          Computer Science at Stony Brook University, expected to graduate in
-          Spring 2024.
+          Hi, there my name is <b>Umair Iqbal</b>. I recently graduated with a
+          Bachelor of Science in Computer Science, Magna Cum Laude, from Stony Brook University.
         </p>
 
         <p className="text">
-          You can click on the links on the left to explore my projects, resume
-          or previous work experiences and to learn more about me. I am always
-          eager to collaborate on an interesting project or talk about a project
-          I have made before so feel free to contact me via email.
+          You can click on the links on the left to explore my projects, resume, 
+          or previous work experiences and to learn more about me. I am always 
+          eager to collaborate on interesting projects or discuss my past work, 
+          so feel free to contact me via email.
         </p>
 
         <p className="text">
-          I am currently looking for a software development internship 
-          where I can contribute code towards a greater purpose, 
-          also gain some skills, and meet some mentors.
+          I am currently seeking a full-time software engineering position where I can contribute 
+          my skills and knowledge to impactful projects.
         </p>
 
         <p className="text">
-          I am passionate about solving problems 
-          through software development whether that’s creating a full-stack web app or a mobile app. 
-          Some of the technologies 
-          I’m proficient with include JavaScript, Python, Java, React, HTML, CSS, Express, MongoDB, and Node
+          I am passionate about solving problems through software development, 
+          whether that’s creating a full-stack web app or a mobile app. 
+          Some of the technologies I am proficient with include JavaScript, Python, Java, React, HTML, CSS, Express, MongoDB, and Node.js.
         </p>
       </div>
     </div>
